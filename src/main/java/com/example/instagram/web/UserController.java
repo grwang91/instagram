bd09205ee757d9f15b470b2e8b344dbfaa674ffd
00321 <003213@naver.com>
2020-05-17 15:42:11 +0900

    비밀번호 암호화 작업 + jwt 기본 설정_real

--- src/main/java/com/example/instagram/web/UserController.java
+++ src/main/java/com/example/instagram/web/UserController.java
@@ -1,5 +1,6 @@
 package com.example.instagram.web;
 
+import com.example.instagram.service.JwtService;
 import com.example.instagram.service.UserService;
 import com.example.instagram.web.dto.UserJoinRequestDto;
 import com.example.instagram.web.dto.UserLoginRequestDto;
@@ -10,21 +11,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 
 @RequiredArgsConstructor      // 초기화 필요한 arg만 constructor
 @RestController
 public class UserController {
 
     private final UserService userService;
+    private final JwtService jwtService;
 
     @PostMapping("/user/join")                //mapping 하나하나마다 swagger 항목 추가
-    public Long join(@RequestBody UserJoinRequestDto userJoinRequestDto) {       //RequestBody -> dto로 알아서 wrapping
+    public Long join(@RequestBody UserJoinRequestDto userJoinRequestDto) throws IllegalAccessException {       //RequestBody -> dto로 알아서 wrapping
         return userService.join(userJoinRequestDto);
     }
 
     @PostMapping("/user/login")
-    public Long login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
-        return userService.login(userLoginRequestDto);
+    public Long login(@RequestBody UserLoginRequestDto userLoginRequestDto, HttpServletResponse response) throws IllegalAccessException {
+        Long userId = userService.login(userLoginRequestDto);
+
+        response.setHeader("AUTHORIZATION", jwtService.create(userId));
+
+        return userId;
     }
 
 }
